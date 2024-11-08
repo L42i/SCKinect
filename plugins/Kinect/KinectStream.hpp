a338f73db800b0b713b527d31a9d73267dad25c9
Evan Murray <emurray49@gatech.edu>
2024-11-07 21:45:49 -0500

    Add Kinect registration to KinectStream

--- plugins/Kinect/KinectStream.hpp
+++ plugins/Kinect/KinectStream.hpp
@@ -7,7 +7,21 @@
 // SuperCollider plug-in API
 #include "SC_PlugIn.hpp"
 
+// Third party API's
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/registration.h>
+
 namespace KinectStream {
+
+struct KinectData {
+    libfreenect2::Freenect2 mFreenect2;
+    libfreenect2::Freenect2Device* mDev;
+    libfreenect2::PacketPipeline* mPipeline;
+    libfreenect2::SyncMultiFrameListener mListener;
+    libfreenect2::FrameMap mFrames;
+};
+
+KinectData gKinectData;
 
 class KinectStream
 {
